203179d221f1968a1a602609bc7d5cef596a0ca6
Abaan9350 <139568623+Abaan9350@users.noreply.github.com>
2024-10-02 19:50:18 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# NoteShare - A Note Sharing Platform
+A Notes Sharing Platform for all the students of SIES, Graduate School of Technology.
---
+++ lib/lightbox/links.php
@@ -0,0 +1,15 @@
+<<<<<<< HEAD
+<?php
+  $links = array(
+    'css' => 'lib/lightbox/css/lightbox.min.css',
+    'js' => 'lib/lightbox/js/lightbox.min.js'
+  );
+?>
+=======
+<?php
+  $links = array(
+    'css' => 'lib/lightbox/css/lightbox.min.css',
+    'js' => 'lib/lightbox/js/lightbox.min.js'
+  );
+?>
+>>>>>>> origin/main
